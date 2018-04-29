9457623785308c076ffe160daea17f7164b9b6c7
Mark Arcitio <marcitio@my.hpu.edu>
2018-04-28 15:53:55 -1000

    added project Folder

---
+++ hello/src/hello/helper.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author 
+ */
+public class helper extends Hello{
+    
+    public void helper(){
+        greeting("this is a helpful message");
+    }
+    
+}
